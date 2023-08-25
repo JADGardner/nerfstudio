a365823e9a14b766d27de45339a8c16151c53c9d
James <iamjamesgardner@googlemail.com>
2023-08-25 08:29:32 +0100

    update slurm

--- slurm.sh
+++ slurm.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=nerfstudio                  # Job name
-#SBATCH --mail-type=END,FAIL                   # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=james.gardner@york.ac.uk   # Where to send mail
-#SBATCH --ntasks=1                             # Run a single task...
-#SBATCH --cpus-per-task=16                     # ...with a single CPU
-#SBATCH --mem=32gb                             # Job memory request
-#SBATCH --time=10:00:00                        # Time limit hrs:min:sec
-#SBATCH --output=cuda_job_%j.log               # Standard output and error log
-#SBATCH --partition=gpu                        # Select the GPU nodes... (test*, interactive, gpu, gpu_big)  
-#SBATCH --gres=gpu:1                           # ...and the Number of GPUs
-
-# This is just printing stuff you don't really need these lines
-echo `date`: executing gpu_test on host $HOSTNAME with $SLURM_CPUS_ON_NODE cpu cores echo 
-cudaDevs=$(echo $CUDA_VISIBLE_DEVICES | sed -e 's/,/ /g')
-echo I can see GPU devices $CUDA_VISIBLE_DEVICES
-echo
+#SBATCH --job-name=reni_neus
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=james.gardner@york.ac.uk
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64gb
+#SBATCH --time=10:00:00
+#SBATCH --output=cuda_job_%j.log
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
 
 source ~/.bashrc
 
