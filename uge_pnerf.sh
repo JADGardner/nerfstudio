24f075f37a3db4b349296892102c711b02007c8f
James Gardner <eey355@frontend8.apocrita>
2023-09-05 11:52:16 +0100

    uge configs

--- uge_pnerf.sh
+++ uge_pnerf.sh
@@ -5,7 +5,7 @@
 #$ -m bea                      # Send email at the beginning and end of the job and if aborted
 #$ -M james.gardner@york.ac.uk # The email address to notify
 #$ -pe smp 8                   # 8 cores (8 cores per GPU)
-#$ -l h_rt=8:0:0               # 1 hour runtime required to run on the short queue
+#$ -l h_rt=1:0:0               # 1 hour runtime required to run on the short queue
 #$ -l h_vmem=11G               # 11 * 8 = 88G total RAM
 #$ -l gpu=1                    # request 1 GPU
 #$ -o output_$JOB_ID.txt       # output file
@@ -14,7 +14,7 @@ source ~/.bashrc
 
 cd /data/home/$USER/code/nerfstudio
 
-TRAINING_COMMAND="ns-train reni-neus --vis wandb"  # Replace with your training command
+TRAINING_COMMAND="ns-train pnerf --vis wandb"  # Replace with your training command
 # TRAINING_COMMAND="cd /data/home/$USER/code/nerfstudio && pip install -e . && cd /data/home/$USER/code/nerfstudio/reni_neus/ns_reni && pip install -e . && cd /data/home/$USER/code/nerfstudio/reni_neus && pip install -e ."
 
 apptainer exec --nv --no-home \
