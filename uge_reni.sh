ea8f0effea6056f848d895bab7242f9d4f8539e6
James Gardner <eey355@frontend8.apocrita>
2023-09-18 11:41:24 +0100

    add reni uge

---
+++ uge_reni.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+DATE_STR=$(date +%Y%m%d_%H%M%S)
+#$ -cwd                                                          # Run job from current directory
+#$ -j y                                                          # Combine stdout and stderr into a single file
+#$ -N reni                                                       # Name of the job
+#$ -m bea                                                        # Send email at the beginning and end of the job and if aborted
+#$ -M james.gardner@york.ac.uk                                   # The email address to notify
+#$ -pe smp 8                                                     # 8 cores (8 cores per GPU)
+#$ -l h_rt=1:0:0                                                 # 1 hour runtime required to run on the short queue
+#$ -l h_vmem=11G                                                 # 11 * 8 = 88G total RAM
+#$ -l gpu=1                                                      # request 1 GPU
+#$ -l gpu_type=ampere                                            # request an Ampere GPU, A100
+#$ -o "outputs/uge_logs/reni/output_${DATE_STR}_$JOB_ID.txt"     # output file
+
+source ~/.bashrc
+
+cd /data/home/$USER/code/nerfstudio
+
+TRAINING_COMMAND="ns-train reni --vis wandb"  # Replace with your training command
+# TRAINING_COMMAND="cd /data/home/$USER/code/nerfstudio && pip install -e . && cd /data/home/$USER/code/nerfstudio/reni_neus/ns_reni && pip install -e . && cd /data/home/$USER/code/nerfstudio/reni_neus && pip install -e ."
+
+apptainer exec --nv --no-home \
+  -B /data/home/$USER:/data/home/$USER \
+  -B /data/home/$USER/.bashrc:/data/home/$USER/.bashrc \
+  -B /data/home/$USER/.config:/data/home/$USER/.config \
+  -B /data/home/$USER/.local:/data/home/$USER/.local \
+  -B /data/home/$USER/.apptainer/pip_venvs:/mnt \
+  -B /data/home/$USER/.vscode-server:/data/home/$USER/.vscode-server \
+  -B /data/home/$USER/.jupyter:/data/home/$USER/.jupyter \
+  --env MPLCONFIGDIR=/data/home/$USER/.config/matplotlib \
+  /data/home/$USER/.apptainer/containers/nerfstudio.sif /bin/bash -c "source /mnt/nerfstudio/bin/activate && $TRAINING_COMMAND"
