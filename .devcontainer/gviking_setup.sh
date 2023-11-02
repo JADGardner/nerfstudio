c372c9724c4d2335f8f28866a595bd13e8a814dd
jadgardner <jadgardner@protonmail.com>
2023-11-02 12:45:31 +0000

    update setup and reni

---
+++ .devcontainer/gviking_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ~/.bashrc
+
+cd /users/$USER/scratch/nerfstudio
+
+python3.10 -m venv /mnt/nerfstduio
+
+source /mnt/nerfstudio/bin/activate
+
+pip install --upgrade pip -y
+
+pip install -e .
+
+cd reni_neus/ns_reni
+
+pip install -e .
+
+cd ..
+
+pip install -e .
+
+pip install git+https://github.com/NVlabs/tiny-cuda-nn.git@v1.6#subdirectory=bindings/torch
